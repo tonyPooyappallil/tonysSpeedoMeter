e09b73a9ca102960f99976c4792de0bf7d7133f9
Antony Chacko <antony101tony@gmail.com>
2023-12-29 21:43:13 +0800

    added a heading

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
       under Development - Tony 
+
+      <h2>Speedometer</h2>
       <LocationSpeedTracker></LocationSpeedTracker>
        
       </header>
